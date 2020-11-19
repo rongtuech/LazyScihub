631dbcc20fc27d2f3e496d4a9b33aa00f93f1782
Trung Ngo <trungngotan94@gmail.com>
2020-11-19 14:55:03 +0900

    Update background.js
    - change URL for scihub site, check https://en.wikipedia.org/wiki/Sci-Hub frequently.

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "open_new_tab" ) {
-      chrome.tabs.create({"url": "https://sci-hub.tw/"+request.url});
+      chrome.tabs.create({"url": "https://sci-hub.st/"+request.url});
     }
   }
 );
